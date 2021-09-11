656e3651719a63ce29e4e895340dc63565a78c75
Charlie <charjhall777@gmail.com>
2021-09-11 16:51:42 -0400

    finish search and add update feature to insert

--- project1/skipList.js
+++ project1/skipList.js
@@ -6,7 +6,7 @@ let skipList = {
 			value: Infinity
 		}]
 	],
-	insert: function(val) {
+	insert: function(val, doc_id, doc_positions) {
 		// search for the position it would be in
 		let val_height = 0;
 		while (Math.random() < probability)
@@ -21,7 +21,10 @@ let skipList = {
 			index = 0;
 
 		// using the top, we can see when it starts working:
-		while (top > -1)
+		while (top > -1) {
+			if (val == skipList.values[top][index].value)
+				skipList.values[top][index].documents += `;${doc_id}|${doc_positions}`
+
 			// if we're on a level that interesects our val_height,
 			// build in the new value
 
@@ -29,20 +32,39 @@ let skipList = {
 				if (top <= val_height)
 					// it will splice in between index and index + 1
 					skipList.values[top].splice(index, 0, {
-						value: val
+						value: val,
+						documents: `${doc_id}|${doc_positions}`
 					});
 				top--;
 			} else
 				index++;
+		}
+	},
+	search: function(val) {
+		let top = skipList.values.length - 1,
+			index = 0;
+
+		while (top >= 0 && skipList.values[top][index].value != val) {
+			// we pretty much do the same thing as insert
+
+			if (val < skipList.values[top][index].value) {
+				top--;
+			} else
+				index++;
+		}
+
+		return !skipList.values[top] ? false : skipList.values[top][index];
 	}
 };
 
-skipList.insert(3);
-skipList.insert(8);
-skipList.insert(20);
-skipList.insert(4);
-skipList.insert(280);
-skipList.insert(19);
-console.log(skipList.values);
+skipList.insert(3, 1, [30, 40, 69]);
+skipList.insert(8, 2, [420, 3, 1]);
+skipList.insert(20, 1, [34, 10, 40]);
+skipList.insert(4, 8, [6, 21, 3]);
+// skipList.insert(280);
+// skipList.insert(19);
+
+
+console.log(skipList.search(3));
 
 //module.exports = skipList;
